f4b2c6f4036e712eb520be2991b35b62c92675db
jake-knox- <jakeknox11@hotmail.co.uk>
2023-05-08 00:07:48 +0100

    starting on sockets work, more style changes for current games

--- index.js
+++ index.js
@@ -11,12 +11,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 // run server normally         -> node index.js
 // run nodemon updating server -> nodemon ./index.js localhost 3000
 
+// game vars
+games = [];
+
+let testGame = {
+    room: "test-room",
+    p1: "",
+    p2: "",
+    moves: 0,
+}
+games.push(testGame)
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
+io.on('connection', (socket) => {
+    console.log('user connected');
 
+    // send user list of active rooms
+    io.emit('get rooms', games);
+
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+
+    socket.on('join room', (room) => {
+        console.log("user joining: " + room);
+        socket.join(room);
+        // io.to(room).emit("user joined", room);
+      });
+
+});
 server.listen(3000, () => {
     console.log('listening on *:3000');
   });
--- public/app.js
+++ public/app.js
@@ -1,5 +1,41 @@
-
-
 var socket = io();
 
+let myRooms = []
 
+// document variables
+const activeGames = document.getElementById("active-games");
+
+
+socket.on('get rooms', function(games) {
+    myRooms = games;
+
+    for (let i = 0; i < myRooms.length; i++) {
+        // logging game room info
+        console.log(myRooms[i].room)
+        console.log(myRooms[i].p1)
+        console.log(myRooms[i].p2)
+        console.log(myRooms[i].moves)
+
+        const newGame = document.createElement("li");
+        newGame.textContent = (`Room: ${myRooms[i].room} | P1: ${myRooms[i].p1} | P2: ${myRooms[i].p2} | moves: ${myRooms[i].moves}`)
+        activeGames.appendChild(newGame);
+    }
+    
+
+
+});
+
+socket.on('update room', function(newDetails) {
+
+    for (let i = 0; i < myRooms.length; i++) {
+        // logging game room info
+        console.log(newDetails[i].room)
+        console.log(newDetails[i].p1)
+        console.log(newDetails[i].p2)
+        console.log(newDetails[i].moves)
+    }
+
+});
+
+//
+socket.emit('join room', "test-room"); 
