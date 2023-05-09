3b7389ede332bf8274e300d6d15143ff6fc4127c
jake-knox- <jakeknox11@hotmail.co.uk>
2023-05-09 11:33:50 +0100

    server work, minor css changes

--- index.js
+++ index.js
@@ -14,39 +14,93 @@ app.use(express.static(path.join(__dirname, 'public')));
 // game vars
 games = [];
 
-let testGame = {
-    room: "test-room",
-    p1: "",
-    p2: "",
-    moves: 0,
-}
-games.push(testGame)
+// let testGame = {
+//     room: "test-room",
+//     p1: "",
+//     p2: "",
+//     moves: 0,
+// }
+// games.push(testGame)
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
+
+const createRoom = (name, userID) => {
+  let newGame = {
+      room: name,
+      p1: userID,
+      p2: "",
+      moves: 0,
+  }
+  games.push(newGame)
+}
+
+const joinRoom = (room, userID) => {
+  
+  for(let i = 0; i < games.length; i ++){
+    //check for the correct room name
+    if(games[i].name == room)
+    {
+      // check for if there is space in the lobby
+      if(games[i].p1 == "")
+      {
+        // no p1, join game
+        games[i].p1 = userID
+      }
+      else if(games[i].p2 == "")
+      {
+        // no p2, join game
+        games[i].p2 = userID
+      }
+      else
+      {
+        // add feature to view a game without playing here?
+        console.log(`Game: ${room} is full. User ${userID} cannot join.`)
+      }
+    }   
+  }
+
+
+
+}
+
 io.on('connection', (socket) => {
-    console.log('user connected');
+    console.log('user connected, id: ' + socket.id);
 
     // send user list of active rooms
-    io.emit('get rooms', games);
+    socket.on('client request rooms', () => {
+      io.emit('get rooms', games);
+    });
 
     socket.on('disconnect', () => {
-      console.log('user disconnected');
+      console.log(`user disconnected, id: ${socket.id}`);
+
+      // disconnect them from server game data?
+      // check for if they are in a game on connect?
+
     });
 
     socket.on('new room', (room) => {
       console.log("new room created: " + room);
       console.log("user " + socket.id + " joining: " + room);
       socket.join(room);
+      createRoom(room, socket.id);
       // io.to(room).emit("user joined", room);
     });
 
     socket.on('join room', (room) => {
-      console.log("user joining: " + room);
-      socket.join(room);
+      console.log("user " + socket.id + " joining: " + room);
+      socket.join(room);      
+      joinRoom(room, socket.id)
       // io.to(room).emit("user joined", room);
+    });
+
+    socket.on('room data', (room) => {      
+      // get actual room data
+      let roomData
+      io.to(room).emit("room data", roomData);
     });
 
     
--- public/app.js
+++ public/app.js
@@ -16,13 +16,13 @@ const activeGames = document.getElementById("active-games");
 form.addEventListener('submit', function(e) {
     e.preventDefault();
     if (input.value) {
-        console.log("form submit");
+        // console.log("form submit");
     socket.emit('new room', input.value);
     input.value = '';
+
+    socket.emit('client request rooms')
     }
 });
-
-
 
 
 
@@ -31,10 +31,10 @@ socket.on('get rooms', function(games) {
 
     for (let i = 0; i < myRooms.length; i++) {
         // logging game room info
-        console.log(myRooms[i].room)
-        console.log(myRooms[i].p1)
-        console.log(myRooms[i].p2)
-        console.log(myRooms[i].moves)
+        // console.log(myRooms[i].room)
+        // console.log(myRooms[i].p1)
+        // console.log(myRooms[i].p2)
+        // console.log(myRooms[i].moves)
 
         const newGame = document.createElement("div");
         newGame.textContent = (`Room: ${myRooms[i].room} | P1: ${myRooms[i].p1} | P2: ${myRooms[i].p2} | moves: ${myRooms[i].moves}`)
@@ -57,5 +57,6 @@ socket.on('update room', function(newDetails) {
 
 });
 
-//
-socket.emit('join room', "test-room"); 
+// testing stuff to run when the page loads
+// socket.emit('join room', "test-room"); 
+socket.emit('client request rooms')
